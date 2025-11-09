391a6cdeef592cfb6b8ebe5220248535e72fc464
Anthony Chae <chaeanthony21@gmail.com>
2025-11-08 23:01:56 -0500

    feat: get by id endpoint

--- routes/boba.js
+++ routes/boba.js
@@ -33,6 +33,21 @@ router.route("/").get(async (req, res) => {
 	}
 });
 
+router.route("/:id").get(async (req, res) => {
+	try {
+		const store = await bobaService.getById(req.params.id);
+		return res.status(200).json(store);
+	} catch (e) {
+		console.error(e);
+
+		if (e instanceof NotFoundError) {
+			return res.status(404).json({ error: e.message });
+		}
+
+		if (e instanceof ValidationError) {
+			return res.status(400).json({ error: e.message });
+		}
+
 		return res.status(500).json({ error: "Internal server error" });
 	}
 });
