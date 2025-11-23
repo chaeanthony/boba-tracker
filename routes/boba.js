51de8688c2d0933959fba43b46a91d08a9855874
Kent Tuneberg <124209135+ktunebe@users.noreply.github.com>
2025-11-23 12:03:54 -0600

    Added sorting options for reviews on store_detail page (#10)

--- routes/boba.js
+++ routes/boba.js
@@ -2,6 +2,7 @@ import express from "express";
 import bobaService from "../data/boba.js";
 import reviewsService from "../data/reviews.js";
 import { NotFoundError, ValidationError } from "../errors.js";
+import { VALID_REVIEW_SORTS } from "../helpers.js";
 
 const router = express.Router();
 
@@ -27,11 +28,49 @@ router.get("/about", (_req, res) => {
 router.get("/stores/:id", async (req, res) => {
 	try {
 		const store = await bobaService.getById(req.params.id);
-		const reviews = await reviewsService.getByStoreId(req.params.id);
+		let reviews = await reviewsService.getByStoreId(req.params.id);
+		// Sort parameter
+		let sort = req.query.sort;
+		// Validate sort parameter
+		if (!VALID_REVIEW_SORTS.includes(sort)) {
+			sort = "most_recent";
+		}
+		// Label to show up in dropdown menu for selected sort
+		let sortLabel = "";
+
+		if (reviews && Array.isArray(reviews)) {
+			reviews = [...reviews];
+
+			switch (sort) {
+				case "least_recent":
+					reviews.sort(
+						(a, b) => new Date(a.updated_at) - new Date(b.updated_at),
+					);
+					sortLabel = "Least Recent";
+					break;
+				case "highest_rated":
+					reviews.sort((a, b) => b.rating - a.rating);
+					sortLabel = "Highest Rated";
+					break;
+				case "lowest_rated":
+					reviews.sort((a, b) => a.rating - b.rating);
+					sortLabel = "Lowest Rated";
+					break;
+				default:
+					reviews.sort(
+						(a, b) => new Date(b.updated_at) - new Date(a.updated_at),
+					);
+					sortLabel = "Most Recent";
+					break;
+			}
+		}
+
 		res.render("store-detail", {
 			title: `${store.name}`,
 			store,
 			reviews,
+			sort,
+			sortLabel,
 		});
 	} catch (e) {
 		console.error(e);
